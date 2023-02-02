d55eb86939c06625f187ab46ab037a4ea1bdeda5
v030v <pjeongin25@gmail.com>
2023-02-02 23:51:54 +0900

    modifided 2884

--- 2884.py
+++ 2884.py
@@ -1,9 +1,9 @@
 H , M = map(int, input().split())
-if H == 0 :
+if H <= 0 :
     if M >= 45 :
         print(H, M-45)
     elif M <= 44 :
-        print(23, M+15)
+        print(H+23, M+15)
 else :
     if M >= 45 :
         print(H, M-45)
