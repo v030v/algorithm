bfa66f97039a937ef153b384ece556af10a1e886
v030v <pjeongin25@gmail.com>
2023-02-22 00:14:49 +0900

    Added 12919

---
+++ 12919.py
@@ -0,0 +1,16 @@
+def solution(seoul):
+    #1 
+    return "김서방은 "+ str(seoul.index("Kim")) +"에 있다"
+
+    #2
+    # k = 0
+    # for i in seoul :
+    #     if i != "Kim" :
+    #         k += 1
+    #     else :
+    #         return "김서방은 "+ str(k) +"에 있다"
+
+    #3
+    # for i in range(len(seoul)) :
+    #     if seoul[i] == "Kim" :
+    #         return "김서방은 "+ str(i) +"에 있다"
