9fb43e84016585ef95d4f8a21476e3504bee8594
v030v <pjeongin25@gmail.com>
2023-02-23 01:02:32 +0900

    Added 12916

---
+++ 12916.py
@@ -0,0 +1,16 @@
+def solution(s):
+    p = 0
+    y = 0
+    for i in range(len(s)) :
+        if s[i] == 'p' or s[i] == 'P':
+            p+= 1
+        elif s[i] == 'y' or s[i] == 'Y' :
+            y+= 1
+    if p == y :
+        return True
+    else :
+        return False
+
+# 실행 값
+print(solution('Pyy'))
+print(solution('Pp000yy'))
