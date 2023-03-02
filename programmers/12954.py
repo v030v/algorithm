14292af779bec4a80c0a5767ef768ca86540cd77
v030v <pjeongin25@gmail.com>
2023-03-03 00:51:11 +0900

    modified 12954

--- programmers/12954.py
+++ programmers/12954.py
@@ -1,15 +1,22 @@
 def solution(x, n):
-    answer = []
-    if x > 0 :
-        for i in range(x,(x*n)+1,x) :
-            answer.append(i)
-    elif x < 0 :
-        for i in range(x,(x*n)-1,x) :
-            answer.append(i)
-    else :
-        for i in range(n) :
-            answer.append(x)
-    return answer
+    return [i * x for i in range(1,n+1)]
+
+# 2
+    # a = [i * x for i in range(1,n+1)]
+    # return a
+
+# 1
+    # answer = []
+    # if x > 0 :
+    #     for i in range(x,(x*n)+1,x) :
+    #         answer.append(i)
+    # elif x < 0 :
+    #     for i in range(x,(x*n)-1,x) :
+    #         answer.append(i)
+    # else :
+    #     for i in range(n) :
+    #         answer.append(x)
+    # return answer
 
 # 실패1
     # answer = []
