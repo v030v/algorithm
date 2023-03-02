ccab3df46830657edf43d18d21f11f2d7e8da8ad
v030v <pjeongin25@gmail.com>
2023-03-02 23:55:49 +0900

    modified 12954

--- programmers/12954.py
+++ programmers/12954.py
@@ -6,7 +6,31 @@ def solution(x, n):
     elif x < 0 :
         for i in range(x,(x*n)-1,x) :
             answer.append(i)
+    else :
+        for i in range(n) :
+            answer.append(x)
     return answer
 
+# 실패1
+    # answer = []
+    # if x > 0 :
+    #     for i in range(x,(x*n)+1,x) :
+    #         answer.append(i)
+    # elif x < 0 :
+    #     for i in range(x,(x*n)-1,x) :
+    #         answer.append(i)
+    # return answer
+
+# 실패2
+    # answer = []
+    # if x > 0 :
+    #     for i in range(x,(x*n)+1,x) :
+    #         answer.append(i)
+    # elif x < 0 :
+    #     for i in range(x,(x*n)-1,x) :
+    #         answer.append(i)
+    # else :
+    #     answer.append(0)
+    # return answer
 print(solution(4,5))
 print(solution(-4,10))
